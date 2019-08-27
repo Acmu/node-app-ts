1a52fd3d7afa8bea880685fa6ed16437dd970703
明远 <zhengmingyuan@sensorsdata.cn>
2019-08-27 17:39:51 +0800

    suc

---
+++ config.ts
@@ -0,0 +1,19 @@
+interface Intl {
+  id: string;
+  msg: string;
+}
+
+const config: Intl[] = [
+  {
+    id: 'iiiii',
+    msg: 'mmm'
+  },
+  {
+    id: 'zzz',
+    msg: 'yyy'
+  }
+];
+
+const namespace = 'fuc';
+
+export { config, Intl, namespace };
--- index.ts
+++ index.ts
@@ -1,3 +1,38 @@
-import * as path from 'path';
+import * as fs from 'fs';
 
-console.log(path.resolve(__dirname, 'src'))
+import { config, Intl, namespace } from './config';
+
+let data: string;
+
+data = `import { getIntl, defineMessages } from '@sc/intl';
+const intl = getIntl()
+
+const messages = defineMessages({
+`;
+
+data += getData(config, namespace);
+
+data += `})
+
+export {
+  intl,
+  messages
+}
+`;
+
+function getData(config: Intl[], namespace: string) {
+  return config.map(v => {
+    return `  ${v.id}: {
+    id: '${namespace}:${v.id}',
+    defaultMessage: '${v.msg}'
+  },
+`;
+  }).join('');
+}
+
+fs.writeFile('intl.js', data, function(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log('write success 😀');
+});
